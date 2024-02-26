4a80813428239b4c893685e19bb4b31c4d71aca9
cmackinnon <calumackinnon@users.noreply.github.com>
2024-02-26 18:17:15 +0000

    Created a class to handle database interactions. An SQLite database is now established in the same directory, with .db files excluded from git via .gitignore.

--- dbinterfacer.py
+++ dbinterfacer.py
@@ -13,62 +13,126 @@ Created on Mon Feb 26 16:11:18 2024
 import sqlite3
 from sqlite3 import Error
 
-def create_connection(database):
-    """ create a database connection to a SQLite database """
-    conn = None
-    try:
-        conn = sqlite3.connect(database)
-        print(sqlite3.version)
-    except Error as e:
-        print(e)
-    finally:
-        if conn:
-            conn.close()
+class DatabaseInterfacer:
+    
+    _databaseConnection = None
+    filename = r"pythonsqlite.db"
 
+    def __init__(self):
+        
+        self._databaseConnection = self.createConnection(self.filename)
+    
+    def __str__(self):
+        
+        return f"A DatabaseInterfacer to {self.filename}"
+    
+    def createConnection(self, filename):
+        
+        """ create a database connection to a SQLite database """
+        
+        conn = None
+        
+        try:
+            conn = sqlite3.connect(filename)
+            print(sqlite3.version)
+            
+            return conn
+            
+        except Error as e:
+            print(e)
+            
+        return conn
+    
 
-def createTablesInThe(database):
-    
-    sqlToCreateTableTasks = """ CREATE TABLE IF NOT EXISTS tasks (
-                                    id INTEGER PRIMARY KEY,
-                                    description TEXT NOT NULL,
-                                    follows INTEGER
-                                    );"""
-    
-    sqlToCreateTableHazards = """ CREATE TABLE IF NOT EXISTS hazards (
-                                    id INTEGER PRIMARY KEY,
-                                    description TEXT NOT NULL
-                                    );"""
-    
-    sqlToCreateTableHierarchyOfControls = """ CREATE TABLE hierarchy_of_controls (
+    def createTables(self):
+        
+        sqlToCreateTableTasks = """ CREATE TABLE IF NOT EXISTS tasks (
+                                        id INTEGER PRIMARY KEY,
+                                        description TEXT NOT NULL,
+                                        follows INTEGER
+                                        );"""
+        
+        sqlToCreateTableHazards = """ CREATE TABLE IF NOT EXISTS hazards (
+                                        id INTEGER PRIMARY KEY,
+                                        description TEXT NOT NULL
+                                        );"""
+        
+        
+        sqlToCreateTableHierarchyOfControls = """ CREATE TABLE IF NOT EXISTS hierarchy_of_controls (
+                                                id INTEGER PRIMARY KEY,
+                                                type TEXT UNIQUE
+                                                );"""
+        
+        
+        sqlToCreateTableCountermeasures = """ CREATE TABLE IF NOT EXISTS countermeasures (
+                                                id INTEGER PRIMARY KEY,
+                                                description TEXT NOT NULL,
+                                                class INTEGER, 
+                                                control_type INTEGER,
+                                                FOREIGN KEY (control_type) REFERENCES hierarchy_of_controls(id)
+                                                );"""
+        
+        sqlToCreateTableLikelihood = """ CREATE TABLE IF NOT EXISTS likelihood (
                                             id INTEGER PRIMARY KEY,
-                                            type TEXT UNIQUE
+                                            task INTEGER,
+                                            hazard INTEGER,
+                                            probability REAL CHECK (probability < 1),
+                                            FOREIGN KEY (task) REFERENCES tasks(id),
+                                            FOREIGN KEY (hazard) REFERENCES hazards(id)
                                             );"""
     
-    sqlToCreateTableCountermeasures = """ CREATE TABLE IF NOT EXISTS countermeasures (
+        sqlToCreateTableMitigations = """ CREATE TABLE IF NOT EXISTS mitigations (
                                             id INTEGER PRIMARY KEY,
-                                            description TEXT NOT NULL,
-                                            class INTEGER, 
-                                            control_type INTEGER FOREIGN KEY
+                                            danger INTEGER,
+                                            countermeasure INTEGER,
+                                            FOREIGN KEY (danger) REFERENCES hazards(id),
+                                            FOREIGN KEY (countermeasure) REFERENCES countermeasures(id)
                                             );"""
+        
+        if self._databaseConnection is None: 
+            print('Could not create the database connection.')
+            return
+        else:        
+            try:
+                
+                dbCursor = self._databaseConnection.cursor()
+                
+                dbCursor.execute(sqlToCreateTableTasks)
+                
+                dbCursor.execute(sqlToCreateTableHazards)
+                
+                print('built 2 tables ')
+                
+                dbCursor.execute(sqlToCreateTableHierarchyOfControls)
     
-    sqlToCreateTableLikelihood = """ CREATE TABLE IF NOT EXISTS likelihood (
-                                        id INTEGER PRIMARY KEY,
-                                        task INTEGER FOREIGN KEY,
-                                        hazard INTEGER FOREIGN KEY,
-                                        probability REAL CHECK (probability < 1)
-                                        );"""
-
-    sqlToCreateTableMitigations = """ CREATE TABLE IF NOT EXISTS mitigations (
-                                        id INTEGER PRIMARY KEY,
-                                        hazard INTEGER FOREIGN KEY,
-                                        countermeasure INTEGER FOREIGN KEY
-                                        );"""
+                dbCursor.execute(sqlToCreateTableCountermeasures)            
+                
+                print('built 4 tables')
+                
+                dbCursor.execute(sqlToCreateTableLikelihood)
+                
+                print('built 5 tables')
+                
+                dbCursor.execute(sqlToCreateTableMitigations)
+                
+            except Error as e:
+                
+                print(e)
+            
+    def closeConnections(self):
+        
+        if self._databaseConnection is not None:
+            self._databaseConnection.close()
 
     
 def main():
     
-    pass
+    dbi = DatabaseInterfacer()
+    dbi.createTables()
+    
+    dbi.closeConnections()
 
+    print('To test, navigate to ' + str(dbi.filename) + ' and run sqlite3 ' + dbi.filename + ', then use the command .tables')
 
 if __name__ == '__main__':
     #create_connection(r"C:\sqlite\db\pythonsqlite.db")
